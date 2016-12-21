f9b31ca85fe0a7ef8d9f98a6db2c011ed661fe54
Teron <teronrussell23@gmail.com>
2016-12-20 19:59:19 -0600

    Long Scrolly Homepage Done
    Included Logo in Navigation to left & menu to the right.
    
    Included animation section instead of hero image using Animate.css.
    
    Included a progress bar for skills.
    
    Added Major Accomplishments, floated images right boxed accomplishment
    title
    
    Added about section
    Image/Description
    
    Created Portfolio php file and added basic HTML skeleton

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Teron Russell | Digital Portfolio</title>
+    <link rel="stylesheet" href="css/circle.css">
+    <link rel="stylesheet" type="text/css" href="css/long-scrolly.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
+
+    <!-- Navigation -->
+
+
+
+    <nav class="main-menu">
+
+        <div class="wrap">
+            <img class="logo" src="images/logo.png" alt="logo" height="50">
+
+            <ul class="navy">
+                <li><a href="#first">Home</a></li>
+                <li><a href="portfolio.php">Portfolio</a></li>
+                <li><a href="#third">Contact</a></li>
+            </ul>
+        </div>
+
+    </nav>
+
+
+    <!-- Top Section -->
+    <div class="hero">
+        <p class="logoz animated fadeInDown">
+            <img src="images/logoalone.png" id="logo" alt="logoAlone" />
+        </p>
+
+        <p class="firstBlock animated fadeInUp">
+            From Photoshop to Unreal Engine, I love to immerse myself in projects that allow me to be creative.
+            <br> I am a designer.
+        </p>
+
+    </div>
+    <section id="first">
+        <div class="container">
+            <h1>Skills</h1>
+
+            <div class="content">
+
+                <div class="column-left">
+                    <div class="c100 p65 yellow medium">
+
+                        <span>65%</span>
+                      
+                        <div class="slice">
+
+                            <div class="bar"></div>
+                            <div class="fill"></div>
+
+                        </div>
+                          
+
+                    </div>
+                    <p>Front End</p>
+                    <p>Web Development</p>
+
+                </div>
+
+
+
+                <div class="column-center">
+                    <div class="content">
+
+                        <div class="column-left">
+                            <div class="c100 p75 green medium">
+
+                                <span>75%</span>
+
+                                <div class="slice">
+
+                                    <div class="bar"></div>
+                                    <div class="fill"></div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                        <p>Photoshop</p>
+                    </div>
+                </div>
+
+
+                    <div class="column-right">
+                        <div class="content">
+
+                            <div class="column-left">
+                                <div class="c100 p55 orange medium">
+
+                                    <span>55%</span>
+
+                                    <div class="slice">
+
+                                        <div class="bar"></div>
+                                        <div class="fill"></div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                        <p>Video Editing</p>
+                    </div>
+                
+
+            </div>
+            <!-- .content -->
+
+        </div>
+        <!-- .container -->
+
+    </section>
+
+    <!-- Second Section -->
+    <section id="second">
+        <div class="container">
+
+            <h1>Latest Work (Major Accomplishments)</h1>
+
+            <div class="content">
+                <article>
+                    <div class="post">
+                        <h2><a href="">Vuforia Map of the Heart, Mind, and Soul</a></h2>
+                            <p><img class="push-right" src="images/Map.jpg" alt="VuforiaMap">Maintaining health for the heart, soul, and mind can be a difficult task to balance. The area around the University of Rochester River campus is completely split by the Genesee river, one side being the 19th Ward and the other being the campus. Our project aims to illustrate the cultural hotspots throughout the region that contribute to the fullness of the heart, soul, and the mind. We plan to do this by using an augmented reality 3D map, with locations in the city that have sentimental value being shown.</p>
+                        
+                            <p>The prototype image shows how users will be able to see a 3D topographical map projected over the marker, a map of the city, on their phone. Users will then be able to navigate and learn about these points of interest, some being on the University campus and others in the city. The purpose of this is to demonstrate how some areas may promote health for the mind better than others, while others are more beneficial for the heart and soul. We plan to do this using the same technology as before; Vuforia and Unity.</p>
+                    </div>
+
+                    <div class="post">
+                        <h2><a href="">UR Hotspot</a></h2>
+                        
+                        <p><img class="push-right" src="images/Hotspot.jpg" alt="UR Hotspot">The purpose of this site is to explore why students go to different spots on campus. Why is one better than the other and what do students do there? Based on a series of interviews with random students on campus, we learned what local spots ー Rettner Hall, Rush Rhees Library, ITS, Gleason Library, Wilson Commons, and Morey Hall ー are best for what activities, whether it be studying, meeting friends, or just hanging out and reading a book. We hope this site gives students ideas about where they should go on campus if they’re looking to get out of their dorm and explore.</p>
+                        
+                        <p>Here is an excert from one of the locations:</p>
+                        <p>Rettner is one of the most recently constructed and innovated study areas on campus. It is located in between Wilson Commons and LeChase and it is open 24 hours. It consists of three floors, which include several study areas, a computer lab with high-end capabilities, an audio and video studio, and a fabrication lab. The space is normally populated throughout the whole school year and it often remains relatively quiet until the afternoon. There are board games,puzzles, and other outlets for entertainment available on the third floor. It is a great spot to utilize if you are doing any form of group work.</p>
+                    </div>
+                    
+                    <div class="butt">
+                       <a href=""> <button class="button" style="vertical-align:middle"><span>See More</span></button></a> 
+                    </div>
+                    
+                </article>
+
+
+
+            </div>
+            <!-- .content -->
+
+        </div>
+        <!-- .container -->
+
+    </section>
+
+    <!-- Third Section -->
+    <section id="third">
+        <div class="container">
+
+            <h1>About</h1>
+
+            <div class="content">
+                <div class="Teron">
+                    <img class="us" src="images/me.jpg" alt="Me">
+                    <p class="name">Teron Russell</p>
+                    <p class="meinfo">I am currently a student at the University of Rochester with the intended major of Digital Media Studies. My career goals are to work in the digital media / digital marketing field as well as continuing my learning and practice of web development both front-end and back. I enjoy working on projects that allow me to use my creative skills. I have a strong disbelief in the saying: "There is only one way to do this." I often find myself trying to find new ways to achieve a task while keeping the steps efficient and fun. In my free time I enjoy spending time with my friends, longboarding, playing videogames, and binge watching different Netflix series. </p>
+
+                </div>
+
+
+
+            </div>
+            <!-- .content -->
+
+        </div>
+        <!-- .container -->
+
+    </section>
+
+
+    <!-- Use the following JavaScript code to do the smooth-scroll thing on the internal links -->
+    <script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script>
+        // From:    Smooth Scrolling by CHRIS COYIER, updated JANUARY 31, 2016
+        //          https://css-tricks.com/snippets/jquery/smooth-scrolling
+        $(function() {
+            $('a[href*="#"]:not([href="#"])').click(function() {
+                if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    if (target.length) {
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 250);
+                        return false;
+                    }
+                }
+            });
+        });
+    </script>
+    <script type="text/javascript" src="js/"></script>
+
+</body>
+
+</html>
