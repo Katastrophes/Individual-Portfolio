c0208ea8b727ca1ab8a9c10552c59d38ac209324
Teron <teronrussell23@gmail.com>
2016-12-31 16:07:28 -0600

    Post FTP update 1
    A few additions & movements (All FTP this data will change moving to the
    new FTP)

--- database-write.php
+++ database-write.php
@@ -1,25 +1,51 @@
 <?php
-    $firstname = Trim(stripslashes($_POST['firstname']));
-    $lastname = Trim(stripslashes($_POST['lastname']));
-    $phone = Trim(stripslashes($_POST['phone']));
+
+    $iname = Trim(stripslashes($_POST['iname']));
+
+    $iemail = Trim(stripslashes($_POST['iemail']));
+
+    $iphone = Trim(stripslashes($_POST['iphone']));
+
+    $imessage = Trim(stripslashes($_POST['imessage']));
+
+
 
     // Database Connection
+
     include 'db-info.php';
+
     $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
+
+
     // Database Query
-    $query = "INSERT INTO friends (first_name, last_name, phone) VALUES ('$firstname', '$lastname', '$phone')";
+
+    $query = "INSERT INTO people (name, email, phone, message) VALUES ('$iname', '$iemail', '$iphone', '$imessage')";
+
     
+
     $result = mysqli_query($connection, $query);
 
+
+
     //Error Checking
+
     $NumberOfRowsAffected = mysqli_affected_rows($connection);
+
     if($NumberOfRowsAffected < 1)
+
     {
+
         die('No records were written to the database. Waaaa!');
+
     }
 
+
+
     // Close Connection & Reditrect
+
     mysqli_close($connection);
-    header("Location: database-read.php");
+
+    header("Location: success.php");
+
 ?>
---
+++ success.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Message Sent</title>
+  </head>
+  <body>
+    <p> Your Message has successfully sent</p>
+    <a href=".">Back to the Index</a>
+  </body>
+</html>
